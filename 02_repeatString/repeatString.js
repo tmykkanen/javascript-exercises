2cd9acdc4cb80e934912526a0343152b1ac9948a
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-08 18:06:54 -0500

    Completed exercises 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  if (num < 0) {
+    return `ERROR`;
+  }
+  newString = ``;
+  
+  for (let i = 0; i < num; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
