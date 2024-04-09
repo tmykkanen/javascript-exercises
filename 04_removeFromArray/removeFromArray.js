4b2001808950415f31ea38e4db416442314c7897
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-08 22:15:11 -0500

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  let cleanArray = array;
+  
+  console.log('array: ' + array)
 
+  for (const element of remove) {
+    cleanArray = cleanArray.filter(item => {
+      return item !== element;
+    });
+  }
+
+  console.log('clean array: ' + cleanArray)
+  
+  return cleanArray;
 };
 
 // Do not edit below this line
