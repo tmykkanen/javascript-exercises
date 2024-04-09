27a47751a57d9bcf1e1ae3622d0bb76ec3b17963
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-09 11:35:16 -0500

    completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  // C = fTemp X 9/5 + 32
+  console.log("ftemp: " + fTemp)
+
+  let C = (fTemp - 32) * (5/9)
+
+  console.log("type before:" + typeof C)
+
+  if (!Number.isInteger(C)) {
+    C = +C.toFixed(1)
+  }
+  
+  console.log("type after:" + typeof C)
+
+  return C;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  
+  let F = cTemp * (9/5) + 32
+
+  if (!Number.isInteger(F)) {
+    F = +F.toFixed(1)
+  }
+  
+  return F;
+
 };
 
 // Do not edit below this line
