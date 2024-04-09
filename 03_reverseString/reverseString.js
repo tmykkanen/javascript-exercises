835f1a10952c0cd98342949a3cdb13634c0ea4c2
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-08 19:04:54 -0500

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const stringArray = string.split("");
+  const stringArrayReversed = stringArray.reverse();
+  return stringArrayReversed.join("");
 };
 
 // Do not edit below this line
